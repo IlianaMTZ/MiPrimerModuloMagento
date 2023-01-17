ee31b213e67da1d55bc79c42283bd9fd10da82f1
IlianaMtz <ilijazmar@gmail.com>
2023-01-17 03:55:39 +0000

    Observer y cron

--- MiprimerCrud/Controller/Index/Test.php
+++ MiprimerCrud/Controller/Index/Test.php
@@ -15,7 +15,11 @@ class Test extends \Magento\Framework\App\Action\Action
 
 	public function execute()
 	{
-		echo "Mi primer modulo";
+		
+		$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Mageplaza'));
+		$this->_eventManager->dispatch('tresdadv_miprimercrud_display_text', ['mp_text' => $textDisplay]);
+		echo $textDisplay->getText();
 		exit;
+
 	}
 }
---
+++ MiprimerCrud/Observer/Product/Data.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Barbanet\SampleModule\Observer\Product;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class Data implements ObserverInterface
+{
+
+    /**
+     * @param \Magento\Framework\Event\Observer $observer
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $product = $observer->getProduct();
+
+        $originalName = $product->getName();
+
+        $modifiedName = $originalName . ' - MODIFICADO POR EL OBSERVER';
+
+        $product->setName($modifiedName);
+    }
+
+}
